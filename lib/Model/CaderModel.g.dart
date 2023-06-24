75f9030e7b08104c8d5ce63b3cd4ea7aeccb69bd
jagadishnallappa <jagadishnallappa@gmail.com>
2023-06-25 00:34:26 +0530

    * CadersPage.dart - Work in progress.

---
+++ lib/Model/CaderModel.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'CaderModel.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+CaderModel _$CaderModelFromJson(Map<String, dynamic> json) => CaderModel(
+      cader: json['cader'] as String?,
+      caderDescription: json['caderDescription'] as String?,
+    );
+
+Map<String, dynamic> _$CaderModelToJson(CaderModel instance) =>
+    <String, dynamic>{
+      'cader': instance.cader,
+      'caderDescription': instance.caderDescription,
+    };
