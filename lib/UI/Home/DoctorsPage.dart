e40c22e02fac6eb964e1ec968fd774db700a85e6
jagadishnallappa <jagadishnallappa@gmail.com>
2023-06-26 23:47:55 +0530

    * DoctorsPage.dart - Work in progress.

--- lib/UI/Home/DoctorsPage.dart
+++ lib/UI/Home/DoctorsPage.dart
@@ -16,6 +16,7 @@ class DoctorsPageState extends State<DoctorsPage>
     with TickerProviderStateMixin {
   final _formKey = GlobalKey<FormState>();
   final Uri _url = Uri.parse("");
+  var oneValue = '';
 
   @override
   void initState() {
@@ -46,33 +47,51 @@ class DoctorsPageState extends State<DoctorsPage>
                 // shrinkWrap: true,
                 itemCount: data.filteredUserModelList.length,
                 itemBuilder: (context, index) {
-                  return ListTile(
-                    leading: const Icon(Icons.account_circle),
+                  return RadioListTile(
+                    value: data.filteredUserModelList[index].firstName,
+                    secondary: const Icon(Icons.account_circle),
+                    groupValue: oneValue,
+                    controlAffinity: ListTileControlAffinity.trailing,
+                    onChanged: (value) {
+                      print(value);
+                      setState(() {
+                        oneValue = value.toString();
+                      });
+                    },
+                    // leading: const Icon(Icons.account_circle),
                     title: Text(
                       "${data.filteredUserModelList[index].firstName!} ${data.filteredUserModelList[index].lastName!}",
                     ),
                     subtitle: Text(
                       data.filteredUserModelList[index].email!,
                     ),
-                    onTap: () async {
-                      data.setSelectedData(data.filteredUserModelList[index]);
-                      // RouteSettings settings =
-                      //     RouteSettings(name: HomePage.route, arguments: '');
-                      // var hasBeenClosed = await Navigator.push(
-                      //   context,
-                      //   MaterialPageRoute(
-                      //       settings: settings,
-                      //       builder: (context) => HomePage(
-                      //             actions: psbsActions.view,
-                      //             subRoute: HomePage.settings,
-                      //           )),
-                      // );
-                    },
+                    // trailing: Radio(
+                    //   value: data.filteredUserModelList[index].firstName,
+                    //   groupValue: oneValue,
+                    //   onChanged: (value) {
+                    //     print(value);
+                    //     setState(() {
+                    //       oneValue = value.toString();
+                    //     });
+                    //   },
+                    // ),
+                    // onTap: (){
+                    //
+                    // }
                   );
                 },
                 separatorBuilder: (_, __) => const Divider(height: 1),
               ),
             ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed:   (){
+          // Navigator.pushNamed(
+          //   context,
+          //   CadersPage.route,
+          // );
+        },
+        label: Text(TelemedStrings.bookNow),
+      ),
     );
   }
 }
