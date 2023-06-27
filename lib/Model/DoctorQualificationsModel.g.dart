c49ec72661460315f58cbdce47dade941ea22514
jagadishnallappa <jagadishnallappa@gmail.com>
2023-06-28 02:04:38 +0530

    * SignUpPage.dart - Work in progress for doctor signUp.

---
+++ lib/Model/DoctorQualificationsModel.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'DoctorQualificationsModel.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+DoctorQualificationsModel _$DoctorQualificationsModelFromJson(
+        Map<String, dynamic> json) =>
+    DoctorQualificationsModel(
+      id: json['id'] as int?,
+      qualification: json['qualification'] as String?,
+    );
+
+Map<String, dynamic> _$DoctorQualificationsModelToJson(
+        DoctorQualificationsModel instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'qualification': instance.qualification,
+    };
