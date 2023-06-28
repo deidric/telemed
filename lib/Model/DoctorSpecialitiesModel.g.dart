8fd6b88a6bb17c514748a625cf912dd0660cdb99
jagadishnallappa <jagadishnallappa@gmail.com>
2023-06-28 17:47:33 +0530

    * BasicInformationPage.dart - Work in progress for doctor signUp.

---
+++ lib/Model/DoctorSpecialitiesModel.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'DoctorSpecialitiesModel.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+DoctorSpecialitiesModel _$DoctorSpecialitiesModelFromJson(
+        Map<String, dynamic> json) =>
+    DoctorSpecialitiesModel(
+      id: json['id'] as int?,
+      speciality: json['speciality'] as String?,
+    );
+
+Map<String, dynamic> _$DoctorSpecialitiesModelToJson(
+        DoctorSpecialitiesModel instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'speciality': instance.speciality,
+    };
