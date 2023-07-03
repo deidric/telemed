52bd2855256ddbbc6a33f4624a9805f80bb8b16d
deidric <deichisukehyuchiha@gmail.com>
2023-07-04 00:04:18 +0800

    * HealthProfileModel.dart - Added model.

--- lib/Model/HealthProfileModel.dart
+++ lib/Model/HealthProfileModel.dart
@@ -1,17 +1,43 @@
 import 'package:json_annotation/json_annotation.dart';
+import 'package:telemed/Model/DrugAllergiesModel.dart';
+import 'package:telemed/Model/MedicalConditionsModel.dart';
+import 'package:telemed/Model/SurgeriesModel.dart';
+import 'package:telemed/Model/SymptomsModel.dart';
 
 part 'HealthProfileModel.g.dart';
 
 @JsonSerializable()
 class HealthProfileModel {
-  int? id;
-  String? cader;
-  String? caderDescription;
+  String? lengthOfFeeling;
+  int? patientId;
+  int? caderId;
+  String? dateOfAppointment;
+  String? timeOfAppointment;
+  String? allergicToDrugsComplaint;
+  String? medicalConditionComplaint;
+  String? surgeryComplaint;
+  int? pwdNumber;
+  int? paymentReferenceNumber;
+  List<SymptomsModel>? symptomsModel;
+  List<DrugAllergiesModel>? drugAllergiesModel;
+  List<MedicalConditionsModel>? medicalConditionsModel;
+  List<SurgeriesModel>? surgeriesModel;
 
   HealthProfileModel({
-    this.id,
-    this.cader,
-    this.caderDescription,
+    this.lengthOfFeeling,
+    this.patientId,
+    this.caderId,
+    this.dateOfAppointment,
+    this.timeOfAppointment,
+    this.allergicToDrugsComplaint,
+    this.medicalConditionComplaint,
+    this.surgeryComplaint,
+    this.pwdNumber,
+    this.paymentReferenceNumber,
+    this.symptomsModel,
+    this.drugAllergiesModel,
+    this.medicalConditionsModel,
+    this.surgeriesModel
   });
 
   factory HealthProfileModel.fromJson(Map<String, dynamic> json) =>
@@ -21,20 +47,53 @@ class HealthProfileModel {
 }
 
 enum FieldNames {
-  id,
-  cader,
-  caderDescription,
+  lengthOfFeeling,
+  patientId,
+  caderId,
+  dateOfAppointment,
+  timeOfAppointment,
+  allergicToDrugsComplaint,
+  medicalConditionComplaint,
+  surgeryComplaint,
+  pwdNumber,
+  paymentReferenceNumber,
+  symptomsModel,
+  drugAllergiesModel,
+  medicalConditionsModel,
+  surgeriesModel
 }
 
 extension FieldNamesExtension on FieldNames {
   String get name {
     switch (this) {
-      case FieldNames.id:
-        return 'id';
-      case FieldNames.cader:
-        return 'cader';
-      case FieldNames.caderDescription:
-        return 'caderDescription';
+      case FieldNames.lengthOfFeeling:
+        return 'lengthOfFeeling';
+      case FieldNames.patientId:
+        return 'patientId';
+      case FieldNames.caderId:
+        return 'caderId';
+      case FieldNames.dateOfAppointment:
+        return 'dateOfAppointment';
+      case FieldNames.timeOfAppointment:
+        return 'timeOfAppointment';
+      case FieldNames.allergicToDrugsComplaint:
+        return 'allergicToDrugsComplaint';
+      case FieldNames.medicalConditionComplaint:
+        return 'medicalConditionComplaint';
+      case FieldNames.surgeryComplaint:
+        return 'surgeryComplaint';
+      case FieldNames.pwdNumber:
+        return 'pwdNumber';
+      case FieldNames.paymentReferenceNumber:
+        return 'paymentReferenceNumber';
+      case FieldNames.symptomsModel:
+        return 'symptomsModel';
+      case FieldNames.drugAllergiesModel:
+        return 'drugAllergiesModel';
+      case FieldNames.medicalConditionsModel:
+        return 'medicalConditionsModel';
+      case FieldNames.surgeriesModel:
+        return 'surgeriesModel';
     }
   }
 }
--- lib/Model/HealthProfileModel.g.dart
+++ lib/Model/HealthProfileModel.g.dart
@@ -8,14 +8,45 @@ part of 'HealthProfileModel.dart';
 
 HealthProfileModel _$HealthProfileModelFromJson(Map<String, dynamic> json) =>
     HealthProfileModel(
-      id: json['id'] as int?,
-      cader: json['cader'] as String?,
-      caderDescription: json['caderDescription'] as String?,
+      lengthOfFeeling: json['lengthOfFeeling'] as String?,
+      patientId: json['patientId'] as int?,
+      caderId: json['caderId'] as int?,
+      dateOfAppointment: json['dateOfAppointment'] as String?,
+      timeOfAppointment: json['timeOfAppointment'] as String?,
+      allergicToDrugsComplaint: json['allergicToDrugsComplaint'] as String?,
+      medicalConditionComplaint: json['medicalConditionComplaint'] as String?,
+      surgeryComplaint: json['surgeryComplaint'] as String?,
+      pwdNumber: json['pwdNumber'] as int?,
+      paymentReferenceNumber: json['paymentReferenceNumber'] as int?,
+      symptomsModel: (json['symptomsModel'] as List<dynamic>?)
+          ?.map((e) => SymptomsModel.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      drugAllergiesModel: (json['drugAllergiesModel'] as List<dynamic>?)
+          ?.map((e) => DrugAllergiesModel.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      medicalConditionsModel: (json['medicalConditionsModel'] as List<dynamic>?)
+          ?.map(
+              (e) => MedicalConditionsModel.fromJson(e as Map<String, dynamic>))
+          .toList(),
+      surgeriesModel: (json['surgeriesModel'] as List<dynamic>?)
+          ?.map((e) => SurgeriesModel.fromJson(e as Map<String, dynamic>))
+          .toList(),
     );
 
 Map<String, dynamic> _$HealthProfileModelToJson(HealthProfileModel instance) =>
     <String, dynamic>{
-      'id': instance.id,
-      'cader': instance.cader,
-      'caderDescription': instance.caderDescription,
+      'lengthOfFeeling': instance.lengthOfFeeling,
+      'patientId': instance.patientId,
+      'caderId': instance.caderId,
+      'dateOfAppointment': instance.dateOfAppointment,
+      'timeOfAppointment': instance.timeOfAppointment,
+      'allergicToDrugsComplaint': instance.allergicToDrugsComplaint,
+      'medicalConditionComplaint': instance.medicalConditionComplaint,
+      'surgeryComplaint': instance.surgeryComplaint,
+      'pwdNumber': instance.pwdNumber,
+      'paymentReferenceNumber': instance.paymentReferenceNumber,
+      'symptomsModel': instance.symptomsModel,
+      'drugAllergiesModel': instance.drugAllergiesModel,
+      'medicalConditionsModel': instance.medicalConditionsModel,
+      'surgeriesModel': instance.surgeriesModel,
     };
