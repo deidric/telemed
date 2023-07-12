dd3abcfbb515ceb1f788e452f300dbe195ca9148
jagadishnallappa <jagadishnallappa@gmail.com>
2023-07-12 22:38:10 +0530

    * ConversationModel.dart - UI and functionality work in progress.

---
+++ lib/Model/ConversationModel.dart
@@ -0,0 +1,86 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'AppointmentModel.g.dart';
+
+@JsonSerializable()
+class ConversationModel {
+  int? id;
+  int? conversationId;
+  int? fromUserId;
+  int? toUserId;
+  String? message;
+  String? timeOfAppointment;
+  String? sentDate;
+  String? readDate;
+  String? attachments;
+  String? fromUserFirstName;
+  String? fromUserLastName;
+  String? toUserFirstName;
+  String? toUserLastName;
+
+  ConversationModel({
+    this.id,
+    this.doctorId,
+    this.patientId,
+    this.caderId,
+    this.dateOfAppointment,
+    this.timeOfAppointment,
+    this.complaint,
+    this.pwdIdNumber,
+    this.pwdIdExpirationDate,
+    this.paymentReferenceNumber,
+    this.firstName,
+    this.lastName,
+  });
+
+  factory ConversationModel.fromJson(Map<String, dynamic> json) =>
+      _$AppointmentModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$AppointmentModelToJson(this);
+}
+
+enum FieldNames {
+  id,
+  doctorId,
+  patientId,
+  caderId,
+  dateOfAppointment,
+  timeOfAppointment,
+  complaint,
+  pwdIdNumber,
+  pwdIdExpirationDate,
+  paymentReferenceNumber,
+  firstName,
+  lastName,
+}
+
+extension FieldNamesExtension on FieldNames {
+  String get name {
+    switch (this) {
+      case FieldNames.id:
+        return 'id';
+      case FieldNames.doctorId:
+        return 'doctorId';
+      case FieldNames.patientId:
+        return 'patientId';
+      case FieldNames.caderId:
+        return 'caderId';
+      case FieldNames.dateOfAppointment:
+        return 'dateOfAppointment';
+      case FieldNames.timeOfAppointment:
+        return 'timeOfAppointment';
+      case FieldNames.complaint:
+        return 'complaint';
+      case FieldNames.pwdIdNumber:
+        return 'pwdIdNumber';
+      case FieldNames.pwdIdExpirationDate:
+        return 'pwdIdExpirationDate';
+      case FieldNames.paymentReferenceNumber:
+        return 'paymentReferenceNumber';
+      case FieldNames.firstName:
+        return 'firstName';
+      case FieldNames.lastName:
+        return 'lastName';
+    }
+  }
+}
